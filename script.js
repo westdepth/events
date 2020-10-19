41e14b06c9a040e1d0ba3b4c5628ced9038299b2
westdepth <44796158+westdepth@users.noreply.github.com>
2020-10-19 22:00:15 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    clicked: function() {
+      this.message = "Hello GAS!"
+    }
+  },
+  created: function(){
+    var self = this
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function()
+    {
+      console.log("1")
+      if(this.readyState==4 && this.status==200)
+      {
+        console.log("2")
+        if(this.response)
+        {
+          self.message = this.response
+          console.log(this.response);
+        }
+      }
+    }
+    xmlHttpRequest.open('GET', 'https://script.googleusercontent.com/macros/echo?user_content_key=QWM1gjVOHexAw57l4InpI1LEVgphIkVItPbzUT5R00Nz6sOJ0DJdP05O--URLl5iIyGpjcVF1CmRXEyhf_Zpbyj13ot6XbONm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMM_m-PkPO14CPt9j2-Ns5MXsMcyTVS2R6qjJeeSma99A6fnhnYWS0v3m5YP8pexOgbU2IR2KBMQ&lib=Mr5SKryE30Nu39veDFTfkPj5oFVHBR1QH',true);
+    xmlHttpRequest.responseType = 'json';
+    xmlHttpRequest.send(null);
+  }
+});
