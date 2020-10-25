fc78bc5d9806933177fc4839fe28c9a23745312d
westdepth <kiri-kiri0513@live.jp>
2020-10-25 11:16:26 +0900

    大学名・卒業年を別メンバ化

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    loading:true,
+    loadingdiv:true,
+    loadingrelay:true,
     message: 'Hello Vue.js!',
-    all : {}
+    isdiv:true,
+    allrelay:{},
+    alldiv:{}
   },
   methods: {
     clicked: function() {
@@ -18,7 +21,7 @@ new Vue({
       }
     },
     getData: function(){
-      this.loading=true;
+      this.loadingdiv=true;
       var self = this
       var xmlHttpRequest = new XMLHttpRequest();
       xmlHttpRequest.onreadystatechange = function()
@@ -29,34 +32,82 @@ new Vue({
           //console.log("2")
           if(this.response)
           {
-            self.all = JSON.parse(JSON.stringify(this.response));
-            for(var shumoku in self.all){
+            self.alldiv = JSON.parse(JSON.stringify(this.response));
+            for(var shumoku in self.alldiv){
               console.log(shumoku);
-              for(var kumi in self.all[shumoku]){
+              for(var kumi in self.alldiv[shumoku]){
                 console.log(""+kumi+"組");
-                console.log(self.all[shumoku][kumi]);
-                for(var lane in self.all[shumoku][kumi]){
-                  console.log("hello");
-                  console.log(self.all[shumoku][kumi][lane].name);
-                  self.all[shumoku][kumi][lane].lap100=self.getTimeDiff(self.all[shumoku][kumi][lane].sp100,self.all[shumoku][kumi][lane].sp50);
-                  self.all[shumoku][kumi][lane].lap200=self.getTimeDiff(self.all[shumoku][kumi][lane].sp200,self.all[shumoku][kumi][lane].sp150);
-                  self.all[shumoku][kumi][lane].lap150=self.getTimeDiff(self.all[shumoku][kumi][lane].sp150,self.all[shumoku][kumi][lane].sp100);
-                  console.log(self.all[shumoku][kumi][lane].lap100);
+                console.log(self.alldiv[shumoku][kumi]);
+                for(var lane in self.alldiv[shumoku][kumi]){
+                  var kojin = self.alldiv[shumoku][kumi][lane];
+                  console.log(kojin.name);
+                  kojin.lap100=self.getTimeDiff(kojin.sp100,kojin.sp50);
+                  kojin.lap200=self.getTimeDiff(kojin.sp200,kojin.sp150);
+                  kojin.lap150=self.getTimeDiff(kojin.sp150,kojin.sp100);
+                  if(kojin.univ!="" && kojin.grad!="" && kojin.grad!=0){
+                    kojin.univgrad="("+kojin.univ+":"+kojin.grad+")";
+                  }else if (kojin.univ!="" || (kojin.grad!="" && kojin.grad!=0)) {
+                    kojin.univgrad="("+kojin.univ+kojin.grad")";
+                  }else {
+                    kojin.univgrad="(-)";
+                  }
+                  kojin.univgrad=kojin.
+                  console.log(kojin.lap100);
                 }
               }
             }
-
-            self.loading=false;
-            console.log(this.all);
+            self.loadingdiv=false;
+            console.log(this.alldiv);
           }
         }
       }
       xmlHttpRequest.open('GET', 'https://script.googleusercontent.com/macros/echo?user_content_key=QWM1gjVOHexAw57l4InpI1LEVgphIkVItPbzUT5R00Nz6sOJ0DJdP05O--URLl5iIyGpjcVF1CmRXEyhf_Zpbyj13ot6XbONm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMM_m-PkPO14CPt9j2-Ns5MXsMcyTVS2R6qjJeeSma99A6fnhnYWS0v3m5YP8pexOgbU2IR2KBMQ&lib=Mr5SKryE30Nu39veDFTfkPj5oFVHBR1QH',true);
       xmlHttpRequest.responseType = 'json';
       xmlHttpRequest.send(null);
+    },
+    getRelay: function(){
+      this.loadingrelay=true;
+      var self = this
+      var xmlHttpRequest = new XMLHttpRequest();
+      xmlHttpRequest.onreadystatechange = function()
+      {
+        //console.log("1")
+        if(this.readyState==4 && this.status==200)
+        {
+          //console.log("2")
+          if(this.response)
+          {
+            self.allrelay = JSON.parse(JSON.stringify(this.response));
+            for(var shumoku in self.allrelay){
+              console.log(shumoku);
+              for(var kumi in self.allrelay[shumoku]){
+                console.log(""+kumi+"組");
+                console.log(self.allrelay[shumoku][kumi]);
+                for(var lane in self.allrelay[shumoku][kumi]){
+                  console.log("hello");
+                  console.log(self.allrelay[shumoku][kumi][lane].name);
+                  self.allrelay[shumoku][kumi][lane].lap100=self.getTimeDiff(self.allrelay[shumoku][kumi][lane].sp100,self.allrelay[shumoku][kumi][lane].sp50);
+                  self.allrelay[shumoku][kumi][lane].lap200=self.getTimeDiff(self.allrelay[shumoku][kumi][lane].sp200,self.allrelay[shumoku][kumi][lane].sp150);
+                  self.allrelay[shumoku][kumi][lane].lap150=self.getTimeDiff(self.allrelay[shumoku][kumi][lane].sp150,self.allrelay[shumoku][kumi][lane].sp100);
+                  console.log(self.allrelay[shumoku][kumi][lane].lap100);
+                }
+              }
+            }
+            self.loadingrelay=false;
+            console.log(this.allrelay);
+          }
+        }
     }
   },
   created: function(){
     this.getData();
+  },
+  computed:{
+    loading: function(){
+      return (isdiv) ? loadingdiv : loadingrelay;
+    },
+    all: function(){
+      return (isdiv) ? alldiv : allrelay;
+    }
   }
 });
