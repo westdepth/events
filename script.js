e85edac98eeba1617e0aab9234a31665c8a40353
westdepth <kiri-kiri0513@live.jp>
2020-10-30 17:23:04 +0900

    jsコメントアウト解除

--- script.js
+++ script.js
@@ -35,8 +35,7 @@ new Vue({
     toggleShow: function(shumoku, kumi){
       //表示しているものを開閉切り替え対象にしているのでちょっと筋が悪い
       //というか組内が配列なのがすでに筋が悪い
-      //一旦保留！！＠＠
-      /*if(this.isdiv){
+      if(this.isdiv){
         this.alldiv[shumoku][kumi].forEach((item, i) => {
           item.show = !item.show;
         });
@@ -44,7 +43,7 @@ new Vue({
         this.allrelay[shumoku][kumi].forEach((item, i) => {
           item.show = !item.show;
         });
-      }*/
+      }
     },
     getData: function(){
       if(this.isdiv){
